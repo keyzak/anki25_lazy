f5254eb196c41263449f9c8a7d5b295c6818babf
keyzak <bipythonme@gmail.com>
2025-02-11 15:23:47 +0100

    Removed config.py and added config.example.py

---
+++ config.example.py
@@ -0,0 +1,9 @@
+# config.example.py
+
+# Replace with your real credentials before running the script
+#that is date to log in anki webb
+EMAIL = "your_email@example.com"
+PASSW = "your_password"
+
+#that is deck name
+DECK_NAME = "Your_Deck_Name"
--- config.py
+++
@@ -1,4 +0,0 @@
-# Credentials
-EMAIL = 'kejzak69@gmail.com'
-PASSW = '2mikolaj2'
-DECK_NAME = 'Google_translator_06_23'
